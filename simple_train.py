9df325fd5015e4341f45bf0aa608b75ef1f86fe4
yindaheng98 <yindaheng98@gmail.com>
2024-01-04 05:43:54 +0000

    strange float error

--- simple_train.py
+++ simple_train.py
@@ -9,10 +9,8 @@ from losses import NeRFLoss
 from apex.optimizers import FusedAdam
 import torch
 from torch.optim.lr_scheduler import CosineAnnealingLR
-from torchmetrics import (
-    PeakSignalNoiseRatio,
-    StructuralSimilarityIndexMeasure
-)
+from torchmetrics import PeakSignalNoiseRatio
+from kornia.utils.grid import create_meshgrid3d
 
 parser = argparse.ArgumentParser()
 parser = models.add_arguements(parser)
@@ -34,15 +32,19 @@ args = parser.parse_args()
 
 device = torch.device('cuda')
 model = models.parse_args(parser)
+G = model.grid_size
+model.register_buffer("density_grid", torch.zeros(model.cascades, G**3))
+model.register_buffer("grid_coords", create_meshgrid3d(G, G, G, False, dtype=torch.int32).reshape(-1, 3))
+
 train_set = datasets.parse_args(parser, split='train')
 train_loader = DataLoader(
     train_set,
-    num_workers=16,
+    num_workers=8,
     persistent_workers=True,
     batch_size=None,
     pin_memory=True)
 
-model = model.to(device)
+model = model.to(device).train()
 net_params = [p for _, p in model.named_parameters()]
 net_loss = NeRFLoss(lambda_distortion=args.distortion_loss_w)
 net_opt = FusedAdam(net_params, args.lr, eps=1e-15)
@@ -51,8 +53,10 @@ train_psnr = PeakSignalNoiseRatio(data_range=1).to(device)
 
 poses = train_set.poses.to(device)
 directions = train_set.directions.to(device)
+model.mark_invisible_cells(train_set.K.to(device), poses, train_set.img_wh)
 for epoch in range(args.num_epochs):
-    for batch in tqdm(train_loader):
+    progress_bar = tqdm(train_loader)
+    for batch in progress_bar:
         rays_o, rays_d = get_rays(directions[batch['pix_idxs']], poses[batch['img_idxs']])
         kwargs = {}
         if args.scale > 0.5:
@@ -65,17 +69,18 @@ for epoch in range(args.num_epochs):
             test_time=False, random_bg=args.random_bg,
             **kwargs)
         batch['rgb'] = batch['rgb'].to(device)
+
         loss_d = net_loss(results, batch)
         if args.use_exposure:
             zero_radiance = torch.zeros(1, 3, device=device)
-            unit_exposure_rgb = model.log_radiance_to_rgb(zero_radiance,
-                                                          **{'exposure': torch.ones(1, 1, device=device)})
-            loss_d['unit_exposure'] = \
-                0.5*(unit_exposure_rgb-train_set.unit_exposure_rgb)**2
+            unit_exposure_rgb = model.log_radiance_to_rgb(zero_radiance, exposure=torch.ones(1, 1, device=device))
+            loss_d['unit_exposure'] = 0.5*(unit_exposure_rgb-train_set.unit_exposure_rgb)**2
         loss = sum(lo.mean() for lo in loss_d.values())
-
         with torch.no_grad():
             psnr = train_psnr(results['rgb'], batch['rgb'])
-        print(psnr, loss)
+        progress_bar.set_description(f"loss {loss} psnr {psnr} lr {net_sch.get_last_lr()}")
+        net_opt.zero_grad()
+        loss.backward()
+    net_sch.step()
 print(model)
 print(len(train_loader))
---
+++ train_simplify.py
@@ -0,0 +1,66 @@
+import torch
+from opt import get_opts
+import os
+import glob
+import imageio
+from pytorch_lightning.callbacks import TQDMProgressBar, ModelCheckpoint
+from pytorch_lightning.loggers import TensorBoardLogger
+
+from utils import slim_ckpt
+from train import NeRFSystem
+
+import warnings
+warnings.filterwarnings("ignore")
+
+
+if __name__ == '__main__':
+    from tqdm import tqdm
+    hparams = get_opts()
+    if hparams.val_only and (not hparams.ckpt_path):
+        raise ValueError('You need to provide a @ckpt_path for validation!')
+    system = NeRFSystem(hparams)
+
+    ckpt_cb = ModelCheckpoint(dirpath=f'ckpts/{hparams.dataset_name}/{hparams.exp_name}',
+                              filename='{epoch:d}',
+                              save_weights_only=True,
+                              every_n_epochs=hparams.num_epochs,
+                              save_on_train_epoch_end=True,
+                              save_top_k=-1)
+    callbacks = [ckpt_cb, TQDMProgressBar(refresh_rate=1)]
+
+    logger = TensorBoardLogger(save_dir=f"logs/{hparams.dataset_name}",
+                               name=hparams.exp_name,
+                               default_hp_metric=False)
+
+    device = torch.device("cuda")
+    system.setup(None)
+    (opts, ), (net_sch, ) = system.configure_optimizers()
+    system.to(device)
+    system.on_train_start()
+    for epoch in range(hparams.num_epochs):
+        progress_bar = tqdm(system.train_dataloader())
+        for batch in progress_bar:
+            batch['rgb'] = batch['rgb'].to(device)
+            with torch.autocast(device_type="cuda"):
+                loss = system.training_step(batch, None)
+                opts.zero_grad()
+                loss.backward()
+            progress_bar.set_description(f"loss {loss} lr {net_sch.get_last_lr()}")
+        net_sch.step()
+
+    if not hparams.val_only:  # save slimmed ckpt for the last epoch
+        ckpt_ = \
+            slim_ckpt(f'ckpts/{hparams.dataset_name}/{hparams.exp_name}/epoch={hparams.num_epochs-1}.ckpt',
+                      save_poses=hparams.optimize_ext)
+        torch.save(ckpt_, f'ckpts/{hparams.dataset_name}/{hparams.exp_name}/epoch={hparams.num_epochs-1}_slim.ckpt')
+
+    if (not hparams.no_save_test) and \
+       hparams.dataset_name == 'nsvf' and \
+       'Synthetic' in hparams.root_dir:  # save video
+        imgs = sorted(glob.glob(os.path.join(system.val_dir, '*.png')))
+        imageio.mimsave(os.path.join(system.val_dir, 'rgb.mp4'),
+                        [imageio.imread(img) for img in imgs[::2]],
+                        fps=30, macro_block_size=1)
+        imageio.mimsave(os.path.join(system.val_dir, 'depth.mp4'),
+                        [imageio.imread(img) for img in imgs[1::2]],
+                        fps=30, macro_block_size=1)
